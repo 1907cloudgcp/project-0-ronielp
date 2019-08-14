8b4a9a48505391580e5fb0690b989a58abd0ee5c
ronielp <roniel@nevada.unr.edu>
2019-08-14 00:47:01 -0400

    Fixed main

--- src/main/python/com/revature/main.py
+++ src/main/python/com/revature/main.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
 import service.service as service
 import logging
-=======
->>>>>>> 472bbfcb6333433b364bb9998f6d4c75b68230f2
 
 '''
 This is your main script, this should call several other scripts within your packages.
 '''
-<<<<<<< HEAD
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     filename='logging_file.log',
@@ -22,10 +18,10 @@ def main():
 
 if __name__ == '__main__':
 	main()
-=======
+
 def main():
 	print('TO-DO')
 
 if __name__ == '__main__':
 	main()
->>>>>>> 472bbfcb6333433b364bb9998f6d4c75b68230f2
+
